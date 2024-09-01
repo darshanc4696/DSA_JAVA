2c899f695a4051b2f6de7f4d65f6e7c05865d106
Darshan Gowda C <86545635+darshanc4696@users.noreply.github.com>
2024-09-01 12:32:22 +0530

    Add files via upload

---
+++ ArraysDS/PaintersPartitionProblem.java
@@ -0,0 +1,63 @@
+package com.ArraysDS;
+
+public class PaintersPartitionProblem 
+{
+	static boolean isPossible(int[] ar, int painters, int m)
+	{
+		int painter = 1, pbc=0;
+		
+		for(int i=0; i<ar.length; i++)
+		{
+			if((ar[i]+pbc) <= m)
+			{
+				pbc += ar[i];
+			}
+			else
+			{
+				painter++;
+				if(painter > painters)
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	static int minTime(int[] ar, int painters, int time)
+	{
+		int l=0, h=0, m=0, res=0;
+		
+		for(int i=0; i<ar.length; i++)
+		{
+			h += ar[i];
+		}
+		
+		while(l<=h)
+		{
+			m = l+(h-l)/2;
+			
+			if(isPossible(ar, painters, m))
+			{
+				res = m;
+				h = m-1;
+			}
+			else
+			{
+				l = m+1;
+			}
+		}
+		
+		return res*time;	
+	}
+
+	public static void main(String[] args) 
+	{
+		int[] boards = {10,20,30,40};
+		int painters = 2;
+		int time = 2;
+		
+		System.out.println(minTime(boards, painters, time));
+	}
+
+}
