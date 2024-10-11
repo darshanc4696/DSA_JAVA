705dbf4a46267ee603fef2e0f777c759ff394233
Darshan Gowda C <86545635+darshanc4696@users.noreply.github.com>
2024-10-11 20:10:28 +0530

    Add files via upload

--- Trees/AllAboutTrees.java
+++ Trees/AllAboutTrees.java
@@ -27,6 +27,42 @@ class Trees
 			inorderTraversal(root.right);
 		}
 	}
+	
+	static void preorderTraversal(Node root)
+	{
+		if(root != null)
+		{
+			System.out.print(root.data+" ");
+			inorderTraversal(root.left);
+			inorderTraversal(root.right);
+		}
+	}
+	
+	static void postorderTraversal(Node root)
+	{
+		if(root != null)
+		{
+			inorderTraversal(root.left);
+			inorderTraversal(root.right);
+			System.out.print(root.data+" ");
+		}
+	}
+	
+	static int heightOf(Node root)
+	{
+		if(root == null)
+		{
+			return 0;
+		}
+		else
+		{
+			int lh = heightOf(root.left);
+			int rh = heightOf(root.right);
+			return (lh > rh ? lh : rh) + 1;
+		}
+	}
+	
+	
 }
 
 
@@ -40,8 +76,9 @@ public class AllAboutTrees {
 		root.right = new Node(40);
 		root.right.left = new Node(50);
 		root.right.right = new Node(60);
+		root.left.left.right = new Node(70);
 		
-		Trees.inorderTraversal(root);
+		System.out.println(Trees.heightOf(root));
 		
 	}
 
